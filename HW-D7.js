488dc1485f66618c0fe43d1809483e6f5ec89c9b
maq10689 <108466005+maq10689@users.noreply.github.com>
2022-07-05 15:04:22 +0100

    Initial commit

---
+++ HW-D7.js
@@ -0,0 +1,142 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+- The solution must be available for the tutors by the end of the day (5PM CET)
+*/
+
+/* EXERCISE 1
+ Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
+
+ */
+
+/* Numbers- Display out number or to make calculations
+Operators - Operator will perform a function between variables
+String - Stores information
+Boolean - true or false value
+Variables - Are used to store information.
+ */
+
+/* EXERCISE 2
+ Try to describe what a variable is, in your own words.
+ */ 
+
+/* 
+Variable is used store information it can be a number, boolean or string data. the variable will be used to 
+perform operations within javascrpit. Which can be displayed using console.log. */
+
+/* EXERCISE 3
+ Write the code to execute an addition (a sum) of the numbers 12 and 20.
+*/
+
+
+
+/* let number2
+
+x = 12
+number2 = 20 
+let calculation
+calculation = x + number2
+console.log("Answer =" + " " + calculation) */
+
+/* EXERCISE 4
+ Create a variable named x and assign to it the number 12.
+*/
+/*let x
+x = 12 */
+
+
+/* EXERCISE 5
+ Create a variable called name and assign to it the string "John Doe".
+*/
+
+/*  */
+/*let Name
+Name = "John Doe"
+console.log(Name);*/
+
+/* EXERCISE 6
+ Execute a subtraction between the number 12 and the variable x you declared before (which is storing the value 12).
+*/ 
+/*let subtraction
+subtraction = x - 12; */
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 7
+ Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 
+ the string "John" (this one has a capital J!)
+ Verify that name1 is different from name2 (HINT: check their equality to be false).
+ Finally, verify that the equality between name1 and name2 becomes true if both are 
+ lowercase (without changing the value of name2).
+*/
+
+
+
+
+/* let name1 
+let name2
+name1 = "john";
+name2 = "John";
+if (name1 !== name2) {
+    console.log("Name is Different");
+} else {
+    console.log("Name is same");
+} */
+
+/* EXERCISE 8
+ Create a variable named x, and assign to it a numeric value less than 10.
+ Write the code to print the literal value of x (ex.: if x is 1, it should print "one", if x is 5 it should print "five" etc.).
+*/
+
+
+
+
+/* let x1
+x1 = 9
+if (x1 === 1) {
+  console.log ("one") ;
+}
+else if (x1 === 2) {
+    console.log ("Two") ;
+}
+else if (x1 === 3) {
+    console.log ("Three");
+   }
+else if (x1 === 4) {
+    console.log ("Four");
+   }
+else if (x1 === 5) {
+    console.log ("Five");
+   }
+else if (x1 === 6) {
+    console.log ("Six");
+   }
+else if (x1 === 7) {
+    console.log ("Seven");
+   }
+else if (x1 === 8) {
+    console.log ("Eight");
+   }
+else if (x1 === 9) {
+    console.log ("Nine");
+}else if (x1 >= 10) 
+ {
+    console.log ("Oopss number too high");
+ }
+ */
+
+/* EXERCISE 9
+ [Extra] Assign a value to a variable using a ternary operator (this topic has not been covered during lesson yet,
+ try to search it by yourself, tomorrow morning we'll discuss it together!)
+*/
+
+
+/* WRITE YOUR ANSWER HERE */
+
+/* WHEN YOU ARE FINISHED
+ Upload the .js file on Eduflow before 5PM CET. In the next days we'll also learn how to use GIT!
+*/
